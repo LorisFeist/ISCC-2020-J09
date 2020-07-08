52f1148458c356c9af3f91679e9e97a7df006731
LorisFeist <67264659+LorisFeist@users.noreply.github.com>
2020-07-08 14:32:36 +0200

    Add files via upload

---
+++ EX-01/mini-site-routing.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<meta charset="utf-8">
+<head><title>mini-site-routing</title> 
+<body>
+<ul>
+      <li> <a href="http://localhost/ISCC/ISCC-2020-J09/EX-01/mini-site-routing.php?page=1">Accueil</a></li>
+      <li> <a href="http://localhost/ISCC/ISCC-2020-J09/EX-01/mini-site-routing.php?page=2">Page2</a></li>
+      <li> <a href="http://localhost/ISCC/ISCC-2020-J09/EX-01/mini-site-routing.php?page=3">Page3</a></li> 
+      <li> <a href="http://localhost/ISCC/ISCC-2020-J09/EX-01/connexion.php?page=connexion">Connexion</a></li> 
+
+<?php 
+if ($_COOKIE['cookie'])
+{
+    echo '<li> <a href="http://localhost/ISCC/ISCC-2020-J09/EX-01/mini-site-routing.php?page=admin">Admin</a>';
+}  
+?>
+
+</head>
+<body>
+
+<form enctype= "multipart/form-data" action = "admin.php" method="post"> 
+    <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+    <input name="userfile" type="file" accept="image/x-png,image/jpg,image/jpeg"/>
+    <input name="description" type="text" placeholder="description" />
+    <input name="titre" type="text" placeholder="Titre" />
+    <input type="submit" value="Envoyer le fichier" />
+</form>
+
+<?php
+
+if($_GET["page"]== "1")
+    {
+        echo "<h1>Accueil</h1>";
+    }
+
+if($_GET["page"]== "2")
+    {
+        echo "<h1>Page 2</h1>";
+    }
+
+if($_GET["page"]== "3")
+    {
+        echo "<h1>Page 3</h1>";
+    }
+if($_GET["page"]== "connexion")
+    {
+        echo "<h1>Connexion</h1>";
+    }
+
+?>
+
+</body>
+
+<footer>
+<?php
+session_start();
+if(isset($_SESSION["id"]))
+    {
+        echo '<p>Login : ' .$_SESSION["id"]. '</p>';
+    }
+elseif(!isset($_COOKIE["id"]))
+    {
+        $_SESSION['id'] = $_POST['login'];
+        $_SESSION['mdp'] = $_POST['password'];
+    }
+else
+    {
+        header('http://localhost/ISCC/ISCC-2020-J09/EX-01/connexion.php?page=connexion');
+    }
+
+?>
+</footer>
+
+</html>
---
+++ EX-01/securite.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+$lien="<a href='http://localhost/ISCC/ISCC-2020-J09/EX-01/connexion.php?page=connexion'>ici</a>";
+$lien2="<a href='http://localhost/ISCC/ISCC-2020-J09/EX-01/mini-site-routing.php?page=1'>ici</a>";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+$login = $_POST['Login'];
+$mdp = $_POST['Password'];
+
+setcookie("cookie", $_SESSION['id'], time()+4600);
+
+if(empty($login) and empty($mdp))
+    {
+        echo "<p>Aucunes données reçues.</p>";
+    }
+elseif($mdp !== "2020")
+    {
+        echo "<h1>Mauvais couple identifiant/mot de passe</h1>";
+        echo "<p>Vous allez être redirigé vers la page de connexion.</p>";
+        echo "<p>Cliquez $lien pour revenir sur la page de connexion.";
+    }
+    
+else 
+    {
+        $_SESSION['id']=$_POST['Login'];
+        echo "<p>Vous allez être redirigé vers la page d'accueil.</p>";
+        echo "<p>Cliquez $lien2 pour revenir sur la page d'accueil.";
+    }
+
+}
+?>
